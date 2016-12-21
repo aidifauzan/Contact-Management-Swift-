747ca67b14b05944ba8ac217defaa0972aa941f0
Aidi Fauzan <aidi.vh@gmail.com>
2016-12-21 21:17:16 +1000

    pods XCode
    uploading pods XCode manifest

---
+++ Pods/Target Support Files/Pods-Contact Management/Pods-Contact Management-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Contact_ManagementVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Contact_ManagementVersionString[];
+
